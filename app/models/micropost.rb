4d7c49c443b00641f02023f5b69932650b542727
bitwise-operations <koroleva.as@inbox.ru>
2019-07-20 20:46:35 +0300

    add validates

--- app/models/micropost.rb
+++ app/models/micropost.rb
@@ -1,4 +1,6 @@
 class Micropost < ApplicationRecord
   belongs_to :user
-  validates :content, length: { maximum: 14 }
+  validates :content, 
+            length: { maximum: 14 },
+            presence: true
 end
