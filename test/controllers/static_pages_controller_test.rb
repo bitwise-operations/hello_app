ecd592d553553651fa3dbc5654ab02eb69311ef4
bitwise-operations <koroleva.as@inbox.ru>
2019-07-25 00:57:50 +0300

    Add a Contact page

--- test/controllers/static_pages_controller_test.rb
+++ test/controllers/static_pages_controller_test.rb
@@ -1,21 +1,30 @@
 require 'test_helper'
 
 class StaticPagesControllerTest < ActionDispatch::IntegrationTest
+  setup do
+    @tutorial_text = 'Ruby on Rails Tutorial Sample App'
+  end
+
   test "should get home" do
     get static_pages_home_url
     assert_response :success
-    assert_select "title", "Home | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Home | #{@tutorial_text}"
   end
 
   test "should get help" do
     get static_pages_help_url
     assert_response :success
-    assert_select "title", "Help | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "Help | #{@tutorial_text}"
   end
   
   test "should get about" do
     get static_pages_about_url
     assert_response :success
-    assert_select "title", "About | Ruby on Rails Tutorial Sample App"
+    assert_select "title", "About | #{@tutorial_text}"
+  end
+
+  test "should get contact" do
+    get static_pages_contact_url
+    assert_response :success
   end
 end
