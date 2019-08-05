fad0f2ce29cfcb06f2da837b79fbd52920a1876d
bitwise-operations <koroleva.as@inbox.ru>
2019-08-05 14:15:43 +0300

    test for title

--- test/integration/site_layout_test.rb
+++ test/integration/site_layout_test.rb
@@ -9,4 +9,14 @@ class SiteLayoutTest < ActionDispatch::IntegrationTest
     assert_select "a[href=?]", about_path
     assert_select "a[href=?]", contact_path
   end
+
+  test 'head signup' do
+    get signup_path
+    assert_select 'h1', 'Sign up'
+    assert_select 'title', full_title('Sign up')
+  end
+
+  test 'full title helper' do
+    assert_equal full_title('Help'), 'Help | Ruby on Rails Tutorial Sample App'
+  end
 end
