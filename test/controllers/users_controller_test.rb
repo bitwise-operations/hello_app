16a217513ecce38bc0890733e0b629e404a6b316
bitwise-operations <koroleva.as@inbox.ru>
2019-11-27 14:10:40 +0300

    generate fake user

--- test/controllers/users_controller_test.rb
+++ test/controllers/users_controller_test.rb
@@ -37,6 +37,11 @@ class UsersControllerTest < ActionController::TestCase
     assert_redirected_to root_url
   end
 
+  test 'should redirect index when not logged in' do
+    get :index
+    assert_redirected_to login_url
+  end
+
   # test "should get new" do
   #   get new_user_url
   #   assert_response :success
